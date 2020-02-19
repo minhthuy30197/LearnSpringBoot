81f5be2d05c3974cf4afa8a008aef5b073f27d79
minhthuy30197 <minhthuy30197@gmail.com>
2020-02-19 22:53:28 +0700

    example hello thymeleaf

---
+++ hello-thymeleaf/src/main/java/com/example/demo/HelloThymeleafApplication.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloThymeleafApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HelloThymeleafApplication.class, args);
+	}
+}
---
+++ hello-thymeleaf/src/main/java/com/example/demo/controller/ThymeleafController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+// Đánh dấu đây là một Controller
+// Nơi tiếp nhận các reqquest từ phía người dùng
+@Controller
+public class ThymeleafController {
+    // Nếu người dùng request GET tới địa chỉ "/"
+    @GetMapping("/")
+    public String index() {
+        // Trả về file index.html
+        return "index";
+    }
+
+    // Nếu người dùng request GET tới địa chỉ "/about"
+    // Model là một object của Spring Boot, được gắn vào trong mọi request
+    @GetMapping("/about")
+    public String index(Model model) {
+        model.addAttribute("name", "Thuy");
+
+        // Trả về file hello.html cùng với thông tin trong object Model
+        return "about";
+    }
+}
