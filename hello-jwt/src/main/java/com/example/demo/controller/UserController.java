9fae530942b183428dc5a5aab32adff939a6e0fd
minhthuy30197 <minhthuy30197@gmail.com>
2020-03-10 09:27:28 +0700

    update hello-jwt example

--- hello-jwt/src/main/java/com/example/demo/controller/UserController.java
+++ hello-jwt/src/main/java/com/example/demo/controller/UserController.java
@@ -1,13 +1,30 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.dto.UserDto;
+import com.example.demo.model.request.CreateUserReq;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 public class UserController {
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/hello")
     public ResponseEntity<?> getProfile() {
         return ResponseEntity.ok("Hello World");
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> createUser(@Valid @RequestBody CreateUserReq req) {
+        UserDto result = userService.createUser(req);
+        return ResponseEntity.ok(result);
     }
 }
