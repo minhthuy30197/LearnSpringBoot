331ff5c3521c54726e2bb2fee1ba2994deb4258f
minhthuy30197 <minhthuy30197@gmail.com>
2020-04-12 17:26:41 +0700

    handle exception

--- user-management-rest-api/src/main/java/com/example/demo/exception/CustomExceptionHandler.java
+++ user-management-rest-api/src/main/java/com/example/demo/exception/CustomExceptionHandler.java
@@ -1,35 +1,35 @@
 package com.example.demo.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<?> handlerNotFoundException(NotFoundException ex, WebRequest req) {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handlerNotFoundException(NotFoundException ex, WebRequest req) {
         // Log err
 
-        ErrorResponse err = new ErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage());
-        return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+        return new ErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage());
     }
 
     @ExceptionHandler(DuplicateRecordException.class)
-    public ResponseEntity<?> handlerDuplicateRecordException(DuplicateRecordException ex, WebRequest req) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handlerDuplicateRecordException(DuplicateRecordException ex, WebRequest req) {
         // Log err
 
-        ErrorResponse err = new ErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage());
-        return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
+        return new ErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage());
     }
 
     // Xử lý tất cả các exception chưa được khai báo
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> handlerException(Exception ex, WebRequest req) {
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handlerException(Exception ex, WebRequest req) {
         // Log err
 
-        ErrorResponse err = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
-        return new ResponseEntity<>(err, HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
     }
 }
