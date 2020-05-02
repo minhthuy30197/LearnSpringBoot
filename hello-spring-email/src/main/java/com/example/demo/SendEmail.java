263628e052cfb813d446f782e4b2e376f014f010
minhthuy30197 <minhthuy30197@gmail.com>
2020-05-02 21:49:21 +0700

    example spring mail

---
+++ hello-spring-email/src/main/java/com/example/demo/SendEmail.java
@@ -0,0 +1,75 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+
+@RestController
+public class SendEmail {
+    @Autowired
+    public JavaMailSender emailSender;
+
+    @GetMapping("/send-simple-email")
+    public String sendSimpleEmail() {
+
+        // Create a Simple MailMessage.
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setTo("abc@gmail.com");
+        message.setSubject("Test Simple Email");
+        message.setText("Hello World");
+
+        // Send Message!
+        emailSender.send(message);
+
+        return "Email Sent!";
+    }
+
+    @GetMapping("/send-attachment-email")
+    public String sendAttachmentEmail() throws MessagingException {
+        // Create a Mime MailMessage.
+        MimeMessage message = emailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setTo("abc@gmail.com");
+        helper.setSubject("Test Attachment Email");
+        helper.setText("Hello World");
+
+        // Add attachment
+        FileSystemResource file = new FileSystemResource(new File( System.getProperty("user.dir") + "\\src\\main\\resources\\static\\avatar.png"));
+        helper.addAttachment(file.getFilename(), file);
+
+        // Send Message!
+        emailSender.send(message);
+
+        return "Email Sent!";
+    }
+
+    @GetMapping("/send-html-email")
+    public String sendHtmlEmail() throws MessagingException {
+        // Create a Mime MailMessage.
+        MimeMessage message = emailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setTo("abc@gmail.com");
+        helper.setSubject("Test Html Email");
+
+        // Add html content
+        String htmlMsg = "<h1 style=\"color:green\">Hello World</h1> <img src='http://www.apache.org/images/asf_logo_wide.gif'>";
+        message.setContent(htmlMsg, "text/html");
+
+        // Send Message!
+        emailSender.send(message);
+
+        return "Email Sent!";
+    }
+}
