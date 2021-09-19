5a775daee35b94ad4fb15ab7761976270c53ff77
thuytm <thuytm@tcbs.com.vn>
2021-09-19 15:45:56 +0700

    send sms with twilio

---
+++ sms-twilio/src/test/java/com/example/smstwilio/SmsTwilioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smstwilio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsTwilioApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
